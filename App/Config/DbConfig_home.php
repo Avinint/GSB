7f02b3f4b1a2ddbaec8faac7dab39355755df080
Avinint <avinint@hotmail.com>
2016-11-10 00:59:58 +0100

    fixed untracked files

--- App/Config/DbConfig_home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	"db_user" => "root",
-	"db_pass" => "",
-	"db_host" => "localhost",
-	"db_name" => "anime",
-    "db_prefix" => "anime_"
-);
--- App/Config/DbConfig_server.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	"db_user" => "root",
-	"db_pass" => "riveton",
-	"db_host" => "172.16.0.5",
-	"db_name" => "gsb",
-	"db_prefix" => ""
-);
