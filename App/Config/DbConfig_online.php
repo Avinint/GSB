dbdb432f3f2645a0c10e9baf338e2e34b5ffede4
Avinint <avinint@hotmail.com>
2016-11-10 00:55:16 +0100

    fixed

--- App/Config/DbConfig_online.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-	"db_user" => "brunoacoxumindao",
-	"db_pass" => "pika4Boo2",
-	"db_host" => "brunoacoxumindao.mysql.db",
-	"db_name" => "brunoacoxumindao",
-    "db_prefix" => "anime_"
-);
