4a37ac48f2c8834c9a51eac3c056014ecd30b171
Avinint <avinint@hotmail.com>
2016-11-12 13:33:58 +0100

    default config file for git

---
+++ App/Config/__dbConfig.php
@@ -0,0 +1,10 @@
+<?php
+/*  Setting par défaut pour un projet wamp, mettre vos paramètres et renommer le fichier en enlevant les underscores */
+
+return array(
+	"db_user" => "root",
+	"db_pass" => "",
+	"db_host" => "localhost",
+	"db_name" => "gsb",
+    "db_prefix" => "",
+	);
