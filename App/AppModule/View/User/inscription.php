cd2bc586b62f656da12f19308bb59244007cf72c
Avinint <avinint@hotmail.com>
2016-11-23 01:03:16 +0100

    css fix

--- App/AppModule/View/User/inscription.php
+++ App/AppModule/View/User/inscription.php
@@ -1,6 +1,7 @@
-
- <div class="form-box">
-     <h1 class="page-title"><?=$page ?></h1>
-     <?=$form->createView(array('submit' => 'S\'inscrire', 'action' => $this->url('utilisateur_signup'))) ?>
- </div>
+<div class = "container-fluid">
+    <div class="form-box">
+    <h1 class="page-title"><?=$page ?></h1>
+    <?=$form->createView(array('submit' => 'S\'inscrire', 'action' => $this->url('utilisateur_signup'))) ?>
+</div>
+</div>
 
